ac58bb67659a73851407dacc3a61b606262acae8
Christian <adrian3384c@gmail.com>
2021-03-22 14:03:54 -0500

    Suebiendo mi primer archivo a mi primer repositorio

---
+++ TiposDatos.js
@@ -0,0 +1,48 @@
+//características de una persona 
+
+const nombre = "Christian";
+const apellidoPaterno = "Chacho";
+const apellidoMaterno = "Jimbo";
+var edad = 16;
+const lugarDenacimiento = "Ecuador";
+
+//Ejercicio: definir los datos característicos de un coche.
+
+const marca = "Suzuki";
+const lugarDefabricación = "Japón";
+var color = "rojo";
+const modelo = "forza";
+const capacidadDepersonas = " Máximo cinco ";
+
+//TIPOS DE DATOS:
+
+var numero1 = 40;
+var numero2 = 13
+
+var texto1 = "pincel";
+var texto2 = "lienzo";
+
+//tipo de dato booleano
+var booleanoVerdadero = true;
+var booleanoFalso = false;
+
+//tipo de dato nulo 
+var condicion = null;
+
+//tipo de dato indefnidio 
+var valorIndefinido;
+console.log ( valorIndefinido );
+
+//tipo de dato ARREGLO:
+const arregloNumeros = [110,114,56,340,100000,2];
+const arregloNombres = [Paul, Ronny, Francisco, Pablo, Jonnathan, Christian];
+const arregloMixto = [Francisco,80,500,Dolores,50,José,65];
+
+//tipo de dato OBJETO:
+var persona = {
+    nombre: "Christian",
+    apellidoPaterno: "Chacho",
+    apellidoMaterno: "Jimbo",
+    edad: 16,
+    lugarDenacimiento: "Ecuador",
+}
